dec924983c1e3e856e21a1a0a22c31698563d189
diepedyt <106111219+diepedyt@users.noreply.github.com>
2022-09-17 22:03:22 +0100

    Update output-Vtku.lua

--- output-Vtku.lua
+++ output-Vtku.lua
@@ -1,3 +1,9 @@
 repeat wait() until game:IsLoaded()
 
+pcall(function()
+  if _G.wl_Key == nil or _G.wl_Key == "" then
+    game.Players.LocalPlayer:Kick("No Key Found")  
+  end
+end)
+
 loadstring(game:HttpGet('https://scripts.luawl.com/11790/YourPapa-AA.lua'))()
