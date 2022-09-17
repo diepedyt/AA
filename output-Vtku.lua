10c119cddc46ae573e22ff3ea3a2d4e63c574d97
diepedyt <106111219+diepedyt@users.noreply.github.com>
2022-09-17 22:03:46 +0100

    Update output-Vtku.lua

--- output-Vtku.lua
+++ output-Vtku.lua
@@ -1,7 +1,7 @@
 repeat wait() until game:IsLoaded()
 
 pcall(function()
-  if _G.wl_Key == nil or _G.wl_Key == "" then
+  if _G.wl_key == nil or _G.wl_key == "" then
     game.Players.LocalPlayer:Kick("No Key Found")  
   end
 end)
